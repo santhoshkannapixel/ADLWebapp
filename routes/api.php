9f99a153b724f1926471f19014603b073a00192f
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-01-06 10:48:45 +0530

    reset_password update

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::get('get-lab-location/{city_id?}',[ApiController::class,'get_lab_location
 Route::get('get-organs',[ApiController::class,'get_organs']);
 Route::get('get-conditions',[ApiController::class,'get_conditions']);
 Route::post('forgot-password',[ApiController::class,'forgot_password']);
-Route::post('reset-password',[ApiController::class,'reset_password']);
+Route::post('reset-password/{id}',[ApiController::class,'reset_password']);
 
 
 Route::post('/patients-consumers', [PatientsConsumersController::class, 'store'])->name('patients-consumers');
