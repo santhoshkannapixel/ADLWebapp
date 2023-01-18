6d284ddff32ac9bb42835c6352afbb3b02a22f3c
santhoshpixel <santhoshd.pixel@gmail.com>
2023-01-18 17:18:10 +0530

    home collection mail sent

---
+++ resources/views/email/home_collection_enquiry.blade.php
@@ -0,0 +1,24 @@
+ 
+              
+              <table class="table table-bordered table-centered m-0 tr-sm table-hover" id="data-table">
+                <tbody>
+                   
+                    <tr>
+                        <td>Name:</td> <td>{{ $details['name'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Mobile:</td> <td>{{ $details['mobile'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Location:</td> <td>{{ $details['location'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Test Name:</td> <td>{{ $details['test_name'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Comments:</td> <td>{{ $details['comments'] }}</td>
+                    </tr>
+                 
+                </tbody>
+               
+            </table>  
