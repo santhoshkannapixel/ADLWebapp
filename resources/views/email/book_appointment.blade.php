44b494138e4850ed1f94c5c220435e489f92816a
santhoshpixel <santhoshd.pixel@gmail.com>
2023-01-18 18:50:55 +0530

    health check up mail sent

---
+++ resources/views/email/book_appointment.blade.php
@@ -0,0 +1,24 @@
+ 
+              
+              <table class="table table-bordered table-centered m-0 tr-sm table-hover" id="data-table">
+                <tbody>
+                    <tr>
+                        <td>Name:</td> <td>{{ $details['name'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Mobile:</td> <td>{{ $details['mobile'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Location:</td> <td>{{ $details['location'] }}</td>
+                    </tr>
+                    <tr>
+                        <td>Test:</td> <td>{{ $details['test'] }}</td>
+                    </tr>
+                   
+                    <tr>
+                        <td>Test Type:</td> <td>{{ $details['test_type'] }}</td>
+                    </tr>
+                 
+                </tbody>
+               
+            </table>  
