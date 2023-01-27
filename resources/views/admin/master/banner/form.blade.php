33b84da62b55690c68f6a24e9e590dfefb545212
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-01-27 16:04:23 +0530

    Banner Title Optional Setup

--- resources/views/admin/master/banner/form.blade.php
+++ resources/views/admin/master/banner/form.blade.php
@@ -1,7 +1,7 @@
 <div class="row mb-3">
     <label class="col-2 text-end col-form-label">Title</label>
     <div class="col-10">
-        {!! Form::text('Title', null, ['class' => 'form-control', 'autocomplete' => 'off', 'required']) !!}
+        {!! Form::text('Title', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
     </div>
 </div>
 <div class="row mb-3">
