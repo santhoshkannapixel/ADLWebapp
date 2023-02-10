5226454be681c27690a0eb449e3bdc4c38ef03f6
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-10 18:01:27 +0530

    Edit View

--- resources/views/admin/master/tests/edit.blade.php
+++ resources/views/admin/master/tests/edit.blade.php
@@ -1,19 +1,19 @@
 @extends('admin.master.layout')
 
 @section('admin_master_content')
-{!! Form::model($data,['route' =>['test.edit' , $data->id] , "Method" => "POST", 'files' => true]) !!}
+    {!! Form::model($data, ['route' => ['test.edit', $data->id], 'Method' => 'POST', 'files' => true]) !!}
     <div class="card custom">
         <div class="card-header">
             <div class="card-title">
-                {{ $data->TestName }}  
+                {{ $data->TestName }}
             </div>
-           
+
             <a href="{{ url()->previous() }}" class="btn btn-primary ms-3">
                 <i class="fa fa-arrow-left me-2" aria-hidden="true"></i>
                 Go back
             </a>
         </div>
-        <div class="card-body"> 
+        <div class="card-body">
             <div class="row mb-3">
                 <label class="col-2 text-end col-form-label">TestId</label>
                 <div class="col-10">
@@ -26,18 +26,13 @@
                     {!! Form::text('TestName', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
                 </div>
             </div>
-            <div class="row mb-3" >
-                <label class="col-2 text-end col-form-label">TestName</label>
-                <div class="col-10"> 
-                    {!! Form::text('TestPrice', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
-                </div>
-            </div>
-            <div class="row mb-3" >
+            <div class="row mb-3">
                 <label class="col-2 text-end col-form-label">Test Images</label>
-                <div class="col-10"> 
+                <div class="col-10">
                     <div class="input-group">
                         <span class="input-group-btn">
-                            <a id="lfm2" data-input="thumbnail2" data-preview="holder2" class="btn btn-primary text-white">
+                            <a id="lfm2" data-input="thumbnail2" data-preview="holder2"
+                                class="btn btn-primary text-white">
                                 <i class="fa fa-picture-o"></i> Choose
                             </a>
                         </span>
@@ -45,42 +40,84 @@
                     </div>
                     <br>
                     <div id="holder2" style="margin-top:15px;max-height:100px;"></div>
-                    @foreach (explode(",",$data->TestImages) as $row)
+                    @foreach (explode(',', $data->TestImages) as $row)
                         <img src="{{ $row }}" width="80px">
                     @endforeach
                 </div>
-                
+
             </div>
         </div>
         <div class="text-end card-footer">
             <a href="{{ route('api_config.index') }}" class="btn btn-light bg-white me-2">back</a>
             <button type="submit" class="btn btn-primary fw-bold">Save</button>
-        </div>  
+        </div>
     </div>
-{!! Form::close() !!}
-@endsection 
+    {!! Form::close() !!}
+@endsection
 
 @section('scripts')
     <script>
         var route_prefix = "{{ url('laravel-filemanager') }}";
     </script>
-    <script src="{{ asset('public/vendor/unisharp/laravel-filemanager/public/js/stand-alone-button.js') }}"></script>
-    <script>$('#lfm').filemanager('file', {prefix: route_prefix});</script>
+    <script>
+        (function($) {
+
+            $.fn.filemanager = function(type, options) {
+                type = type || 'file';
+
+                this.on('click', function(e) {
+                    var route_prefix = (options && options.prefix) ? options.prefix : '/filemanager';
+                    var target_input = $('#' + $(this).data('input'));
+                    var target_preview = $('#' + $(this).data('preview'));
+                    window.open(route_prefix + '?type=' + type, 'FileManager', 'width=900,height=600');
+                    window.SetUrl = function(items) {
+                        var file_path = items.map(function(item) {
+                            return item.url;
+                        }).join(',');
+
+                        // set the value of the desired input to image url
+                        target_input.val('').val(file_path).trigger('change');
+
+                        // clear previous preview
+                        target_preview.html('');
+
+                        // set or change the preview image src
+                        items.forEach(function(item) {
+                            target_preview.append(
+                                $('<img>').css('height', '5rem').attr('src', item.thumb_url)
+                            );
+                        });
+
+                        // trigger change event
+                        target_preview.trigger('change');
+                    };
+                    return false;
+                });
+            }
+
+        })(jQuery);
+    </script>
+    <script>
+        $('#lfm').filemanager('file', {
+            prefix: route_prefix
+        });
+    </script>
     <script>
         var lfm = function(id, type, options) {
             let button = document.getElementById(id);
 
-            button.addEventListener('click', function () {
+            button.addEventListener('click', function() {
 
                 var route_prefix = (options && options.prefix) ? options.prefix : '/filemanager';
                 var target_input = document.getElementById(button.getAttribute('data-input'));
                 var target_preview = document.getElementById(button.getAttribute('data-preview'));
 
-                window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager', 'width=900,height=600');
+                window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager',
+                    'width=900,height=600');
 
-                window.SetUrl = function (items) {
-                    
-                    var file_path = items.map(function (item) {
+                window.SetUrl = function(items) {
+
+                    var file_path = items.map(function(item) {
                         return item.url;
                     }).join(',');
 
@@ -92,7 +129,7 @@
                     target_preview.innerHtml = '';
 
                     // set or change the preview image src
-                    items.forEach(function (item) {
+                    items.forEach(function(item) {
                         let img = document.createElement('img')
                         img.setAttribute('style', 'height: 5rem')
                         img.setAttribute('src', item.thumb_url)
@@ -104,6 +141,8 @@
                 };
             });
         };
-        lfm('lfm2', 'file', {prefix: route_prefix});
+        lfm('lfm2', 'file', {
+            prefix: route_prefix
+        });
     </script>
 @endsection
