73d013d7b63bc71c4e877b0ccc0586761fc91a07
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-17 18:07:09 +0530

    Next Data Api Update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -413,7 +413,7 @@ class ApiController extends Controller
             "status"    => true,
             "count"     => count($Tests),
             "data"      => $Tests,
-            "next_data" => $request->limit ?? 10 == count($Tests) ? true : false
+            "next_data" => $request->limit ?? 10 >= count($Tests) ? true : false
         ];
     }
 
