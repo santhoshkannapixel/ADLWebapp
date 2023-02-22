4227cfc917852e086f4e6064e98b84821d24953c
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-22 11:49:24 +0530

    API Update

--- resources/views/admin/sections/breadcrumb.blade.php
+++ resources/views/admin/sections/breadcrumb.blade.php
@@ -104,7 +104,7 @@
     </nav>
     <div class="dropdown ms-3 me-3 border-start ps-3">
         <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="http://www.staroceans.org/w3c/img_avatar.png" alt="" width="32" height="32" class="rounded-5 me-2">
+            <img src="https://www.staroceans.org/w3c/img_avatar.png" alt="" width="32" height="32" class="rounded-5 me-2">
         </a>
         <ul class="dropdown-menu dropdown-menu-dark bg-my-dark text-small shadow" aria-labelledby="dropdownUser1" style="">
             <li><a class="dropdown-item" href="#"><b>{{ Sentinel::getUser()->name }}</b> <small class="ms-2 badge bg-success text-white">{{ Sentinel::getUser()->roles[0]->name }}</small></a></li>
