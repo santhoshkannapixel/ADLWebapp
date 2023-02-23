3e5387ba5f62acf31b7263d116feff3e0a65f2e9
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-23 11:39:30 +0530

    Post method Update

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('locations', [ApiController::class,'locations']);
 Route::get('banners', [ApiController::class,'banners']);
-Route::get('topBookedTest', [ApiController::class,'topBookedTestPackages']);
+Route::post('topBookedTest', [ApiController::class,'topBookedTestPackages']);
 Route::post('test/{slug}', [ApiController::class,'testDetails']);
 Route::any('test-list', [ApiController::class,'testLists']);
 Route::post('bannerContactForm', [ApiController::class,'bannerContactForm']);
