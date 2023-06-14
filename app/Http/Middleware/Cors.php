bf7eebf66b0a8cc630cf358986c520ca1dd1c0de
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-06-14 19:25:58 +0530

    handle

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,11 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Content-Type', 'application/json')
+            ->header('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('X-Requested-With', '*');
     }
 }
