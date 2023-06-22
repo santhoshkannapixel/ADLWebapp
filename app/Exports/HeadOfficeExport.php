0ca15351f98585b8a3d479bc9e14c2f12d05775a
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-06-22 18:42:29 +0530

    FEAT : Export Update

--- app/Exports/HeadOfficeExport.php
+++ app/Exports/HeadOfficeExport.php
@@ -3,17 +3,24 @@
 namespace App\Exports;
 
 use App\Models\HeadOffice;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class HeadOfficeExport implements FromCollection,WithHeadings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    public $request;
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
     public function collection()
     {
-        return HeadOffice::select('name','email','mobile','company_name','designation','message','address','created_at')->get();
+        return  HeadOffice::select('name','email','mobile','company_name','designation','message','address','created_at')->when(!empty($this->request->start_date) && !empty($this->request->end_date), function ($query) {
+            $start_month     = Carbon::parse($this->request->start_date)->startOfDay();
+            $end_month       = Carbon::parse($this->request->end_date)->endOfDay();
+            $query->whereBetween('created_at', [$start_month, $end_month]);
+        })->get();
     }
     public function headings(): array
     {
--- app/Http/Controllers/Admin/HeadOfficeController.php
+++ app/Http/Controllers/Admin/HeadOfficeController.php
@@ -6,6 +6,7 @@ use App\Exports\HeadOfficeExport;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\HeadOffice;
+use Carbon\Carbon;
 use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Laracasts\Flash\Flash;
 use Illuminate\Support\Facades\Log;
@@ -19,7 +20,11 @@ class HeadOfficeController extends Controller
     {
         if ($request->ajax()) {
 
-            $data = HeadOffice::orderBy('id', 'DESC');
+            $data = HeadOffice::when(!empty($request->start_date) && !empty($request->end_date), function ($query) use ($request) {
+                $start_month     = Carbon::parse($request->start_date)->startOfDay();
+                $end_month       = Carbon::parse($request->end_date)->endOfDay();
+                $query->whereBetween('created_at', [$start_month, $end_month]);
+            })->orderBy('id', 'DESC');
 
             return DataTables::eloquent($data)
                 ->addIndexColumn()
@@ -65,7 +70,6 @@ class HeadOfficeController extends Controller
     }
     public function exportData(Request $request)
     {
-        return Excel::download(new HeadOfficeExport, 'head_office.xlsx');
-
+        return Excel::download(new HeadOfficeExport($request), 'head_office.xlsx');
     }
 }
