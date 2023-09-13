3e583f4b2bb24789ad6ba686dab79a19eb3067f0
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-09-13 19:03:56 +0530

    FEAT : 02 Update

--- resources/views/admin/enquiry/feedback/show.blade.php
+++ resources/views/admin/enquiry/feedback/show.blade.php
@@ -27,17 +27,39 @@
                     @endif
                 @endforeach
                 @if (!is_null($data->qa_comments))
-                @if (strstr($data['page_url'], 'feedback-b2b'))
-                    @foreach (json_decode($data->qa_comments) as $key => $row)
-                        <tr>
-                            <td width="25%"><strong>{{ ucfirst($row['question']) }}</strong>:</td>
-                            <td width="5%" class="text-center">:</td>
-                            <td width="65%"> {{ $row['answer'] }}</td>
-                        </tr>
-                    @endforeach
-                @endif
+                    @if (strstr($data['page_url'], 'feedback-b2b'))
+                        @foreach (json_decode($data->qa_comments) as $key => $row)
+                            <tr>
+                                <td width="25%"><strong>{{ ucfirst($row->question) }}</strong>:</td>
+                                <td width="5%" class="text-center">:</td>
+                                <td width="65%"> {{ $row->answer }}</td>
+                            </tr>
+                        @endforeach
+                    @endif
                 @endif
             </table>
+            @if (!strstr($data['page_url'], 'feedback-b2b'))
+                <table cellpadding="10" cellspacing="10">
+                    <thead>
+                        <tr>
+                            <th>Q.No</th>
+                            <th>Question</th>
+                            <th>Answer</th>
+                            <th>Comment </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach (json_decode($data->qa_comments) as $key => $row)
+                            <tr>
+                                <td>{{ $key + 1 }}</td>
+                                <td>{{ $row->question }}</td>
+                                <td>{{ $row->answer == 1 ? 'Yes' : 'No' }}</td>
+                                <td>{{ $row->comments ?? '-' }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
         </div>
     </div>
 @endsection
